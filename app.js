bd166efaeabfba9a335f1826e452978c018265cf
AhmedFaisal11 <ahmedbinfaisal555@gmail.com>
2022-01-19 14:16:16 +0500

    build.yaml file changed

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const morgan = require('morgan');
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.use(morgan("common"));
 
 app.get('/' , (req , res ) => {
     res.status(200).json({
